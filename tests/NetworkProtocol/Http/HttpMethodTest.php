920c8d3dbbc7fe2c215d56c715d91a493ee0ce99
sebastiankennedy <sebastiankennedy@foxmail.com>
2021-12-16 23:51:19 +0800

    Update HttpMethodTest.php file

---
+++ tests/NetworkProtocol/Http/HttpMethodTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Luyiyuan\Toolkits\Tests\NetworkProtocol\Http;
+
+use Luyiyuan\Toolkits\Tests\TestCase;
+
+class HttpMethodTest extends TestCase
+{
+
+}
