0413a795df1b8cc58b6d62b0f22dc731978704ad
sebastiankennedy <sebastiankennedy@foxmail.com>
2022-01-01 21:31:25 +0800

    Add ArrayHelper.php.

---
+++ src/Helpers/ArrayHelper.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Luyiyuan\Toolkits\Helpers;
+
+use Illuminate\Support\Arr;
+
+class ArrayHelper extends Arr
+{
+
+}
---
+++ tests/Helpers/ArrayHelperTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Luyiyuan\Toolkits\Tests\Helpers;
+
+use Luyiyuan\Toolkits\Tests\TestCase;
+
+class ArrayHelperTest extends TestCase
+{
+}
