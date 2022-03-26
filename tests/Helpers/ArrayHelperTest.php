d64b0e0a1de60e602c08f56d2de68af7287760c2
sebastiankennedy <sebastiankennedy@foxmail.com>
2022-03-26 10:50:13 +0800

    add swap test

--- tests/Helpers/ArrayHelperTest.php
+++ tests/Helpers/ArrayHelperTest.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Luyiyuan\Toolkits\Tests\Helpers;
 
+use Luyiyuan\Toolkits\Helpers\ArrayHelper;
 use Luyiyuan\Toolkits\Tests\TestCase;
 
 class ArrayHelperTest extends TestCase
 {
-    public function test_something(): void
+    public function test_swap(): void
     {
-        $this->markTestIncomplete();
+        $data = [0, 1, 2];
+        ArrayHelper::swap($data, 1, 2);
+        $this->assertSame($data, [0, 2, 1]);
+
+        $name = ['kennedy', 'sebastian'];
+        ArrayHelper::swap($name, 0, 1);
+        $this->assertSame($name, ['sebastian', 'kennedy']);
     }
 }
