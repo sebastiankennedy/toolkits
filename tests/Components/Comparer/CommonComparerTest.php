a93159fd09257dd247fcccd9e490d3d54bb7cb86
SebastianKennedy <sebastiankennedy@foxmail.com>
2023-02-15 12:04:29 +0800

    add CommonComparerTest.php

---
+++ tests/Components/Comparer/CommonComparerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Luyiyuan\Toolkits\Tests\Components\Comparer;
+
+use Luyiyuan\Toolkits\Tests\TestCase;
+
+class CommonComparerTest extends TestCase
+{
+    public function testConstruct(): void
+    {
+    }
+}
