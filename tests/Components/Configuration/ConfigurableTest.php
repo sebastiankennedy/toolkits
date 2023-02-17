2f8239f3f85fd8e5123f9721a5b911dc2347130f
SebastianKennedy <sebastiankennedy@foxmail.com>
2023-02-17 12:05:48 +0800

    add ConfigurableTest.php

---
+++ tests/Components/Configuration/ConfigurableTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Luyiyuan\Toolkits\Tests\Components\Configuration;
+
+use Luyiyuan\Toolkits\Tests\TestCase;
+
+class ConfigurableTest extends TestCase
+{
+}
